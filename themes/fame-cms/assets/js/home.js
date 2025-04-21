f3a2a0f8a1f2f41fbe13b5368c931723617f81ac
ehlwas <myramkenacc@gmail.com>
2025-04-21 23:55:02 +0400

    update

--- themes/fame-cms/assets/js/home.js
+++ themes/fame-cms/assets/js/home.js
@@ -1,15 +1,15 @@
-const video = document.getElementById('foundersVideo');
+// const video = document.getElementById('foundersVideo');
 
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      video.play();
-    } else {
-      video.pause();
-    }
-  });
-}, {
-  threshold: 0.5 // Adjust how much of the video must be visible
-});
+// const observer = new IntersectionObserver(entries => {
+//   entries.forEach(entry => {
+//     if (entry.isIntersecting) {
+//       video.play();
+//     } else {
+//       video.pause();
+//     }
+//   });
+// }, {
+//   threshold: 0.5 // Adjust how much of the video must be visible
+// });
 
-observer.observe(video);
+// observer.observe(video);
