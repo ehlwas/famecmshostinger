8022e4c51bfc2c5135787147fd29fceab5e0bb1c
ehlwas <myramkenacc@gmail.com>
2025-04-23 23:17:42 +0400

    test lottie

---
+++ themes/fame-cms/assets/js/index-bodymovin.js
@@ -0,0 +1,17 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('animation1'),
+    renderer: 'svg',
+    loop: false,
+    autoplay: true,
+    path: '/themes/fame-cms/assets/animation-json/animation-1.json',
+    // rendererSettings: {
+    //   preserveAspectRatio: 'none' // <- THIS breaks aspect ratio lock
+    // }
+})
+  
+function handlePlayAnimation() {
+    // animation.setSpeed(0.5);
+    animation.play();
+    // animation.loop = false;
+    // animation.autoplay = true;
+}
