da537de822c63a5b4949780a9bec272a04941c85
ehlwas <myramkenacc@gmail.com>
2025-04-24 00:01:18 +0400

    added animation until 6

--- themes/fame-cms/assets/js/index-bodymovin.js
+++ themes/fame-cms/assets/js/index-bodymovin.js
@@ -1,13 +1,18 @@
-var animation = bodymovin.loadAnimation({
-    container: document.getElementById('animation1'),
-    renderer: 'svg',
-    loop: false,
-    autoplay: true,
-    path: '/themes/fame-cms/assets/animation-json/animation-1.json',
-    rendererSettings: {
-      preserveAspectRatio: 'none' // <- THIS breaks aspect ratio lock
-    }
-})
+
+const animationSetup = (container, animationName) => {
+
+    return bodymovin.loadAnimation({
+        container,
+        renderer: 'svg',
+        loop: false,
+        autoplay: false,
+        path: `/themes/fame-cms/assets/animation-json/${animationName}.json`,
+        rendererSettings: {
+          preserveAspectRatio: 'none' // <- THIS breaks aspect ratio lock
+        }
+    })
+
+}
   
 function handlePlayAnimation() {
     // animation.setSpeed(0.5);
@@ -15,3 +20,148 @@ function handlePlayAnimation() {
     // animation.loop = false;
     // animation.autoplay = true;
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    const animationOne = document.getElementById("animation1");
+    const animationTwo = document.getElementById("animation2");
+    const animationThree = document.getElementById("animation3");
+    const animationFour = document.getElementById("animation4");
+    const animationFive = document.getElementById("animation5");
+    const animationSix = document.getElementById("animation6");
+    const animationSeven = document.getElementById("animation7");
+
+
+
+    if (animationOne) {
+        const bodymovinAnimation = animationSetup(animationOne, 'animation-1')
+        
+        const observer = new IntersectionObserver(function (entries, observer) {
+            entries.forEach(function(entry) {
+                if (entry.isIntersecting) {
+                    bodymovinAnimation.play();
+                    observer.unobserve(entry.target);
+                }
+            });
+        }, {
+            threshold: 0.5
+        });
+        
+        observer.observe(animationOne);
+    }
+
+
+
+    if (animationTwo) {
+        const bodymovinAnimation = animationSetup(animationTwo, 'animation-2')
+        
+        const observer = new IntersectionObserver(function (entries, observer) {
+            entries.forEach(function(entry) {
+                if (entry.isIntersecting) {
+                    bodymovinAnimation.play();
+                    observer.unobserve(entry.target);
+                }
+            });
+        }, {
+            threshold: 0.5
+        });
+        
+        observer.observe(animationTwo);
+    }
+
+
+
+    if (animationThree) {
+        const bodymovinAnimation = animationSetup(animationThree, 'animation-3')
+        
+        const observer = new IntersectionObserver(function (entries, observer) {
+            entries.forEach(function(entry) {
+                if (entry.isIntersecting) {
+                    bodymovinAnimation.play();
+                    observer.unobserve(entry.target);
+                }
+            });
+        }, {
+            threshold: 0.5
+        });
+        
+        observer.observe(animationThree);
+    }
+
+
+
+    if (animationFour) {
+        const bodymovinAnimation = animationSetup(animationFour, 'animation-4')
+        
+        const observer = new IntersectionObserver(function (entries, observer) {
+            entries.forEach(function(entry) {
+                if (entry.isIntersecting) {
+                    bodymovinAnimation.play();
+                    observer.unobserve(entry.target);
+                }
+            });
+        }, {
+            threshold: 0.5
+        });
+        
+        observer.observe(animationFour);
+    }
+
+
+
+    if (animationFive) {
+        const bodymovinAnimation = animationSetup(animationFive, 'animation-5')
+        
+        const observer = new IntersectionObserver(function (entries, observer) {
+            entries.forEach(function(entry) {
+                if (entry.isIntersecting) {
+                    bodymovinAnimation.play();
+                    observer.unobserve(entry.target);
+                }
+            });
+        }, {
+            threshold: 0.5
+        });
+        
+        observer.observe(animationFive);
+    }
+
+
+
+    if (animationSix) {
+        const bodymovinAnimation = animationSetup(animationSix, 'animation-6')
+        
+        const observer = new IntersectionObserver(function (entries, observer) {
+            entries.forEach(function(entry) {
+                if (entry.isIntersecting) {
+                    bodymovinAnimation.play();
+                    observer.unobserve(entry.target);
+                }
+            });
+        }, {
+            threshold: 0.5
+        });
+        
+        observer.observe(animationSix);
+    }
+
+
+
+    if (animationSeven) {
+        const bodymovinAnimation = animationSetup(animationSeven, 'animation-7')
+        
+        const observer = new IntersectionObserver(function (entries, observer) {
+            entries.forEach(function(entry) {
+                if (entry.isIntersecting) {
+                    bodymovinAnimation.play();
+                    observer.unobserve(entry.target);
+                }
+            });
+        }, {
+            threshold: 0.5
+        });
+        
+        observer.observe(animationSeven);
+    }
+    
+})
